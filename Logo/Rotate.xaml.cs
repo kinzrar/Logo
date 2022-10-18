80d438437c44a6aede613fc11a520e91cef0db81
Дима Чернявский <dimasikok-2003@mail.ru>
2022-10-19 00:09:50 +0300

    Добавьте файлы проекта.

---
+++ Logo/Rotate.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Logo
+{
+    /// <summary>
+    /// Interaction logic for Rotate.xaml
+    /// </summary>
+    public partial class Rotate : Window
+    {
+        public Rotate()
+        {
+            InitializeComponent();
+        }
+
+        private int rotation = 0;
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += new EventHandler(Rotation);
+            timer.Start();
+        }
+
+        private void Rotation(object sender, EventArgs e)
+        {
+            rotation+=10;
+            Hypno.RenderTransform = new RotateTransform(rotation);
+        }
+    }
+}
